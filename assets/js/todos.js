9ceee525f014844f15efb77c6cdfe86d61680dd8
Ayush Khanna <ayushkhanna1996@gmail.com>
2018-08-01 20:03:48 +0530

    functionality completed

---
+++ assets/js/todos.js
@@ -0,0 +1,27 @@
+//Checked off items
+//We used on("click") instead of click to reflect changes on future li's
+$("ul").on("click", "li", function(){
+	$(this).toggleClass("completed");
+});
+
+//Delete Todo when X is clicked
+$("ul").on("click", "span", function(event){
+	//Below line is possible only because of jQuery
+	//It allows parents to be removed with the help of child
+	$(this).parent().fadeOut(500, function(){
+		$(this).remove();
+	});
+	event.stopPropagation();
+});
+
+//Adding Todo
+$("input[type='text']").keypress(function(event){
+	//which is the property of keypress that enables to select key by its value specified in jQuery pages.
+	if (event.which === 13) {
+		//fetching text from input option
+		var todoText = $(this).val();
+		$(this).val("");
+		//create a new li and add to ul
+		$("ul").append("<li><span>X</span> " + todoText + "</li>");
+	}
+});
